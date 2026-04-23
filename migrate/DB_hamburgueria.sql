a71201803b32ab2054bb41bfec31fa50784121b9
Maria-Touzo <maria.touzo@aluno.senai.br>
2026-04-23 17:20:05 -0300

    salvando o bcd

--- migrate/DB_hamburgueria.sql
+++ migrate/DB_hamburgueria.sql
@@ -9,11 +9,22 @@ create table if not exists hamburgueres (codigo int  AUTO_INCREMENT PRIMARY KEY,
  foto varchar(100),
  disponibilidade bool);
  
-create table if not exists usuario (codigo int  AUTO_INCREMENT PRIMARY KEY,
- nome_usuario varchar(20), 
- senha varchar(200));
- 
+create table if not exists usuarios (codigo int  AUTO_INCREMENT PRIMARY KEY,
+ usuario varchar(20), 
+ senha varchar(200),
+ nome VARCHAR(40));
+  
+  INSERT INTO hamburgueres(produto, descricao, preco, destaque, foto, disponibilidade) VALUES
+  ('Classic Burger', 'Pão, carne 160g, queijo e maionese.', '25.9', '1', 'https://source.unsplash.com/600x400/?classic,burger', '1'
+ 'Bacon Blast', 'Hambúrguer com fatias crocantes de bacon.', '32', '1', 'https://source.unsplash.com/600x400/?bacon,burger', '1'
+ 'Veggie Delight', 'Hambúrguer de grão-de-bico e salada.', '28.5', '0', 'https://source.unsplash.com/600x400/?veggie,burger', '1'
+ 'Monster Cheese', 'Triplo queijo cheddar e cebola caramelizada.', '35.9', '1', 'https://source.unsplash.com/600x400/?cheese,burger', '1'
+ 'Chicken Crisp', 'Frango empanado, alface e molho especial.', '24', '0', 'https://source.unsplash.com/600x400/?crispy,chicken,burger', '1'
+ 'Smash Simples', 'Prensado na chapa com cebola e picles.', '18', '0', 'https://source.unsplash.com/600x400/?smash,burger', '1'
+ 'Hot Jalapeño', 'Carne, queijo e pimenta jalapeño.', '29.9', '0', 'https://source.unsplash.com/600x400/?jalapeno,burger', '0'
+'Gourmet Trufado', 'Carne angus com maionese trufada.', '45', '1', 'https://source.unsplash.com/600x400/?gourmet,burger', '1');
 
+ 
 
 ALTER TABLE hamburgueres 
 MODIFY COLUMN descricao VARCHAR(255);
@@ -21,15 +32,6 @@ MODIFY COLUMN descricao VARCHAR(255);
 ALTER TABLE hamburgueres 
 MODIFY COLUMN foto VARCHAR(1000);
  
- INSERT INTO hamburgueres (produto, descricao, preco, destaque, foto, disponibilidade) VALUES
-('Classic Burger', 'Pão, carne 160g, queijo e maionese.', 25.90, true, 'classic.jpg', true),
-('Bacon Blast', 'Hambúrguer com fatias crocantes de bacon.', 32.00, true, 'bacon_blast.jpg', true),
-('Veggie Delight', 'Hambúrguer de grão-de-bico e salada.', 28.50, false, 'veggie.jpg', true),
-('Monster Cheese', 'Triplo queijo cheddar e cebola caramelizada.', 35.90, true, 'monster.jpg', true),
-('Chicken Crisp', 'Frango empanado, alface e molho especial.', 24.00, false, 'chicken.jpg', true),
-('Smash Simples', 'Prensado na chapa com cebola e picles.', 18.00, false, 'smash.jpg', true),
-('Hot Jalapeño', 'Carne, queijo e pimenta jalapeño.', 29.90, false, 'hot.jpg', false),
-('Gourmet Trufado', 'Carne angus com maionese trufada.', 45.00, true, 'trufado.jpg', true);
 
-INSERT INTO usuario (usuario, senha) VALUES 
+
  
